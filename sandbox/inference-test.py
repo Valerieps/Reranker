a0c7c81cbf21e15cb41cf1c58f02b6d3e0acd397
valeria souza <souza.pereira.valeria@gmail.com>
2021-08-10 10:25:45 -0300

    reraanged files

--- sandbox/inference-test.py
+++
@@ -1,5 +0,0 @@
-from reranker import RerankerForInference
-rk = RerankerForInference.from_pretrained("Luyu/bert-base-mdoc-bm25")  # load checkpoint
-
-inputs = rk.tokenize('weather in new york', 'it is cold today in new york', return_tensors='pt')
-score = rk(inputs).logits
